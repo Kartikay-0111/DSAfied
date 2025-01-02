30cf4681e98d4530d44d3419bda5df315ca04226
Tanish2207 <bhamare.tn@gmail.com>
2025-01-02 17:08:40 +0530

    feat: Add minor animations

--- client/src/components/on-board-form/OnboardForm.jsx
+++ client/src/components/on-board-form/OnboardForm.jsx
@@ -1,11 +1,29 @@
 import "./on-board-form.css";
 import React, { useState } from "react";
+import { SplitText } from "../SplitText";
+
+
 
 const Form1 = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+    console.log(selectedImage);
+    
+  };
+
   return (
     <div>
       <div className="px-10 py-10 flex justify-between items-end  text-white">
-        <h1 className="text-2xl">Get Started</h1>
+        <SplitText text="Get Started" className="text-2xl font-bold leading-7" delay={10} />
       </div>
       <div className="flex justify-center items-center flex-col gap-4 px-4">
         <div
@@ -16,7 +34,7 @@ const Form1 = () => {
           <input
             type="text"
             placeholder="Enter your username"
-            className="w-3/4 bg-transparent focus:outline-none text-white"
+            className="text-base w-full bg-transparent focus:outline-none text-white"
           />
         </div>
         <div
@@ -27,15 +45,37 @@ const Form1 = () => {
           <input
             type="text"
             placeholder="Enter your name"
-            className="w-3/4 bg-transparent focus:outline-none text-white"
+            className="text-base w-3/4 bg-transparent focus:outline-none text-white"
           />
         </div>
+        <p className="text-[hsl(0,0%,70%)] self-start">Edit your avatar</p>
         <div
-          className="my-3 mx-4 flex justify-between items-center gap-4 bg-[#010A14] px-3 py-2  w-full "
+          className="mb-3 mx-4 flex justify-between items-center gap-4 bg-[#010A14] px-3 py-2  w-full "
           style={{ borderRadius: "5px", border: "1px solid #A8A8A8" }}
         >
-          <img src="./dummy_user_template.png" />
-          <p className="text-white opacity-50 flex-1">Edit your avatar</p>
+          {/* <img src="./dummy_user_template.png" /> */}
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            id="avatar-upload"
+          />
+          <label htmlFor="avatar-upload" className="cursor-pointer">
+            {selectedImage ? (
+              <img
+                src={selectedImage}
+                alt="Selected Avatar"
+                className="w-12 h-12 bg-[hsl(212,90%,8%)] rounded-full object-cover"
+              />
+            ) : (
+              <img
+                src="./dummy_user_template.png"
+                alt="Default Avatar"
+                className="w-12 h-12 bg-[hsl(212,90%,8%)] rounded-full object-cover"
+              />
+            )}
+          </label>
+          {/* <p className="text-white opacity-50 flex-1">Edit your avatar</p> */}
         </div>
       </div>
     </div>
@@ -51,8 +91,8 @@ const Form2 = () => {
   ];
   return (
     <div>
-      <div className="px-10 py-10 flex justify-between items-end  text-white">
-        <h1 className="text-base md:text-2xl">Other Platforms</h1>
+      <div className="px-10 py-10 flex gap-3 justify-between items-end  text-white">
+        <SplitText text="Other Platforms" className="text-base md:text-2xl font-bold leading-7" delay={10} />
         <p className="text-sm md:text-base opacity-50">{username}</p>
       </div>
       {platforms.map((platform, index) => (
@@ -89,17 +129,20 @@ const Form3 = () => {
   return (
     <div>
       <div className="px-10 py-10 flex justify-between items-end  text-white">
-        <h1 className="text-base md:text-2xl">Difficulty preference</h1>
+        <SplitText text="Difficulty preference" className="text-base md:text-2xl font-bold leading-7" delay={10} />
         <p className="text-sm md:text-base opacity-50">{username}</p>
       </div>
-      <h2 className="text-white opacity-80">
+      <h2 className="text-white text-center w-full opacity-80 px-8">
         What difficulty level questions are you comfortable with?
       </h2>
 
       <div className="flex justify-center my-4 gap-2">
         {difficulties.map((difficulty, index) => (
-          <div onClick={() => difficulty.isSelected = true}
-            className={`text-white opacity-80 rounded-lg px-5 py-2  hover:bg-[#171e41] ${difficulty.isSelected ? 'bg-[#67BBFF]' : 'bg-[#010A14]'} `}
+          <div
+            onClick={() => (difficulty.isSelected = true)}
+            className={`text-white opacity-80 rounded-lg px-5 py-2  hover:bg-[#171e41] ${
+              difficulty.isSelected ? "bg-[#67BBFF]" : "bg-[#010A14]"
+            } `}
             style={{ border: "1px solid #A8A8A8" }}
           >
             {difficulty.level}
@@ -178,18 +221,13 @@ const OnboardForm = () => {
           <div className="bg-[#BFBFBF70] h-2 flex items-center justify-center gap-2 px-3 py-4 rounded-full mt-4 mb-10">
             {forms.map((_, index) => (
               <div
-                className={`bg-black w-3 h-3 cursor-pointer rounded-full ${
+                className={`bg-black w-3 h-3 cursor-pointer rounded-full transition-opacity duration-500 ${
                   activeFormIndex === index ? "" : "opacity-30"
                 }`}
                 onClick={() => navigateTo(index)}
               ></div>
             ))}
           </div>
-          {/* <div className="bg-[#BFBFBF70] h-2 flex items-center justify-center gap-2 px-3 py-4 rounded-full mt-4 mb-10">
-            <div className="bg-black w-3 h-3  cursor-pointer rounded-full"></div>
-            <div className="bg-black w-3 h-3  cursor-pointer rounded-full opacity-30"></div>
-            <div className="bg-black w-3 h-3  cursor-pointer rounded-full opacity-30"></div>
-          </div> */}
         </div>
       </div>
     </div>
