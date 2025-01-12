e4154dc6036115a7811c7da6ed3f87d699069cc3
Tanish2207 <bhamare.tn@gmail.com>
2025-01-12 18:25:58 +0530

    refactor: cleanup before merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,12 @@ import { fetchCodeforcesData, prepareChartData, fetchPlatformRatings } from './c
 
 function App() {
   const [ratingHistory, setRatingHistory] = useState([]);
-  const [username, setUsername] = useState('Chef-KTK');
+  const [graphUsername, setGraphUsername] = useState('Chef-KTK');
 
   const [platforms, setPlatforms] = useState([
     { name: 'LeetCode', logo: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/LeetCode_Logo_1.png', username: 'johndoe123', rating: 1500, solved: 250 },
     { name: 'CodeChef', logo: 'https://user-images.githubusercontent.com/63710339/185728318-0b976716-4f78-4a0a-a377-1643cc18a57e.png', username: 'chefmaster99', rating: 1700, solved: 300 },
-    { name: 'Codeforces', logo: 'https://img.icons8.com/?size=160&id=jldAN67IAsrW&format=png', username: username, rating: null, solved: null },
+    { name: 'Codeforces', logo: 'https://img.icons8.com/?size=160&id=jldAN67IAsrW&format=png', username: graphUsername, rating: null, solved: null },
     { name: 'GeeksForGeeks', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/GeeksforGeeks.svg/640px-GeeksforGeeks.svg.png', username: 'geekcoder', rating: 1400, solved: 150 },
   ]);
 
@@ -24,14 +24,11 @@ function App() {
   };
 
   useEffect(() => {
-    fetchCodeforcesData(username).then(data => setRatingHistory(data));
-  }, [username]);
+    fetchCodeforcesData(graphUsername).then(data => setRatingHistory(data));
+  }, [graphUsername]);
 
-  useEffect(() => {
-    fetchPlatformRatings(platforms).then(updatedPlatforms => setPlatforms(updatedPlatforms));
-  }, []);
 
-  const chartData = prepareChartData(ratingHistory, username);
+  const chartData = prepareChartData(ratingHistory, graphUsername);
 
   return (
     <div>
